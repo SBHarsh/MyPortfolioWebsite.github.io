0098dede13b9c5dc52d95d2d4d354614d3aef6b1
SBHarsh <87004714+SBHarsh@users.noreply.github.com>
2023-09-04 20:30:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["UI/UX Designer", "Developer", "Digital Artist", "Freelancer"],
+        strings: ["Software Engineer", "Developer", "UI/UX Designer", "Digital Artist", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["UI/UX Designer", "Developer", "Digital Artist", "Freelancer"],
+        strings: ["Software Engineer", "Developer", "UI/UX Designer", "Digital Artist", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
